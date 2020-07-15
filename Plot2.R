c5a726989d016c051310035ec28e85ca480c7f68
rohitj1702 <68003181+rohitj1702@users.noreply.github.com>
2020-07-15 13:18:07 +0530

    Delete Plot2.R

--- Plot2.R
+++
@@ -1,8 +0,0 @@
-tdata<-'household_power_consumption.txt'
-mydata<-read.csv2(tdata , header = TRUE,sep = ";")
-plot(mydata$Global_active_power, type="l", ylab="Global Active Power (Kilowatts)", xlab="Day",axes=F)
-
-png("Plot2.png",width=480, height=480)
-plot(mydata$Global_active_power, type="l", ylab="Global Active Power (Kilowatts)", xlab="Day",axes=F)
-
-dev.off()
